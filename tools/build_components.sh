2d1c40ab636415146652eb70c99cd1698d26c023
Maarten Ectors <maarten@greentic.ai>
2026-02-07 12:28:17 +0000

    fixing failing tests

--- tools/build_components.sh
+++ tools/build_components.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-PACKAGES=("provision" "questions" "secrets-probe" "slack" "teams" "telegram" "webchat" "webex" "webex-webhook" "whatsapp" "messaging-ingress-slack" "messaging-ingress-teams" "messaging-ingress-telegram" "messaging-ingress-whatsapp" "messaging-provider-dummy" "messaging-provider-telegram" "messaging-provider-teams" "messaging-provider-email" "messaging-provider-slack" "messaging-provider-webex" "messaging-provider-whatsapp" "messaging-provider-webchat")
+PACKAGES=("provision" "questions" "secrets-probe" "slack" "teams" "telegram" "telegram-webhook" "webchat" "webex" "webex-webhook" "whatsapp" "messaging-ingress-slack" "messaging-ingress-teams" "messaging-ingress-telegram" "messaging-ingress-whatsapp" "messaging-provider-dummy" "messaging-provider-telegram" "messaging-provider-teams" "messaging-provider-email" "messaging-provider-slack" "messaging-provider-webex" "messaging-provider-whatsapp" "messaging-provider-webchat")
 
 for package in "${PACKAGES[@]}"; do
   bash "${ROOT_DIR}/tools/build_components/${package}.sh"
